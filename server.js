dc53e869d0f1a84f53a601a5002d50aa149e515f
Mayur290 <mayurbansal98@gmail.com>
2021-12-11 17:06:27 +0530

    validation setup for register post request

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import routes from "./routes";
 import { APP_PORT } from "./config";
+import errorHandler from "./middlewares/errorHandler";
 const app = express();
 
+app.use(express.json());
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
   res.send("hi");
 });
 
+app.use(errorHandler);
 app.listen(APP_PORT, () => {
   console.log(`listening at port ${APP_PORT}`);
 });
